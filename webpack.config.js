08f42cbfb51a0fa738f0a00b4a48ed2ef90c2a6f
Cristiano Almeida <cs.almeida@protonmail.ch>
2018-06-11 16:49:52 +0100

    Gets all available SCSS themes and transpiles them to css files.

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const path = require('path');
+const fs = require('fs');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const setEntryPoints = (themes = {}) => {
+  fs.readdirSync(path.join(__dirname, 'src')).forEach(file => {
+    themes[file.slice(0, -5)] = path.join(__dirname, 'src', file)
+  })
+  return themes
+}
+
+const themes = setEntryPoints();
+
+module.exports = {
+  mode: process.env.MODE,
+  entry: themes,
+  output: {
+    filename: 'main.js',
+    path: path.join(__dirname, 'themes')
+  },
+  module: {
+  // Add loader
+  rules: [{
+      test: /\.(scss)$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader', 'sass-loader'
+      ]
+    }]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    })
+  ],
+};
